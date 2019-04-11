c72c3f21f13ae217743b3518a745c51129458de5
Fela Maslen <felamaslen@gmail.com>
2019-04-11 21:14:26 +0100

    Added update and delete routes for cycle routes

--- src/routes/cycle-routes/index.js
+++ src/routes/cycle-routes/index.js
@@ -1,28 +1,61 @@
 import { Router } from 'express';
+import { applyPatch } from 'fast-json-patch';
+import clone from 'clone-deep';
+import joi from 'joi';
 
 import { catchAsyncErrors, clientError } from '~/modules/error-handling';
 import { validate } from '~/modules/validate';
-import { add } from '~/schemas/route';
+import { add, patch } from '~/schemas/route';
+
+async function getById(db, id) {
+  const [item] = await db.select()
+    .from('routes')
+    .where({ id });
+
+  return item;
+}
 
 function processRouteResponse(item) {
-  const { mid_lon: midLon, mid_lat: midLat, ...route } = item;
+  const {
+    points,
+    mid_lon: midLon,
+    mid_lat: midLat,
+    ...route
+  } = item;
 
   return {
     ...route,
+    points: JSON.parse(points),
     midPoint: [midLon, midLat],
+  };
+}
+
+function makeRouteDocument(input) {
+  const {
+    origin,
+    destination,
+    points,
+    midPoint: [midLon, midLat],
+    length,
+    bearing,
+  } = input;
+
+  return {
+    origin,
+    destination,
+    points: JSON.stringify(points),
+    mid_lon: midLon,
+    mid_lat: midLat,
+    length: Math.round(length),
+    bearing,
   };
 }
 
 function routeCreate(config, logger, db) {
   return catchAsyncErrors(async (req, res) => {
-    const {
-      origin,
-      destination,
-      points,
-      midPoint: [midLon, midLat],
-      length,
-      bearing,
-    } = req.validBody;
+    const route = makeRouteDocument(req.validBody);
+
+    const { origin, destination } = route;
 
     const uniqueExists = await db.select()
       .from('routes')
@@ -32,23 +65,13 @@ function routeCreate(config, logger, db) {
       throw clientError('A duplicate route exists with the same origin and destination');
     }
 
-    const [id] = await db.insert({
-      origin,
-      destination,
-      points: JSON.stringify(points),
-      mid_lon: midLon,
-      mid_lat: midLat,
-      length: Math.round(length),
-      bearing,
-    })
+    const [id] = await db.insert(route)
       .returning('id')
       .into('routes');
 
-    const addedRoute = await db.select()
-      .from('routes')
-      .where({ id });
+    const addedRoute = await getById(db, id);
 
-    res.json(processRouteResponse(addedRoute));
+    res.status(201).json(processRouteResponse(addedRoute));
   });
 }
 
@@ -61,15 +84,65 @@ function routeRead(config, logger, db) {
       return res.json(cycleRoutes.map(processRouteResponse));
     }
 
-    const [cycleRoute] = await db.select()
-      .from('routes')
-      .where({ id: req.params.id });
-
+    const cycleRoute = await getById(db, req.params.id);
     if (!cycleRoute) {
       throw clientError('Route not found', 404);
     }
 
     return res.json(processRouteResponse(cycleRoute));
+  });
+}
+
+function routeUpdate(config, logger, db) {
+  return catchAsyncErrors(async (req, res) => {
+    if (!(req.body && Array.isArray(req.body))) {
+      throw clientError('Invalid patch');
+    }
+
+    const oldRouteRow = await getById(db, req.params.id);
+    if (!oldRouteRow) {
+      throw clientError('Route not found', 404);
+    }
+
+    const oldRoute = processRouteResponse(oldRouteRow);
+
+    let newDocument = null;
+    try {
+      ({ newDocument } = applyPatch(clone(oldRoute), req.body));
+    } catch (err) {
+      throw clientError('Invalid patch');
+    }
+
+    const { error } = joi.validate(newDocument, patch(oldRoute));
+    if (error) {
+      throw clientError(`Invalid data: ${error.message}`);
+    }
+
+    const { id, ...newRoute } = newDocument;
+
+    await db('routes')
+      .update(makeRouteDocument(newRoute))
+      .where({ id: req.params.id });
+
+    res.status(204).end();
+  });
+}
+
+function routeDelete(config, logger, db) {
+  return catchAsyncErrors(async (req, res) => {
+    const [{ count }] = await db.select(db.raw('count(*) as count'))
+      .from('routes')
+      .where({ id: req.params.id });
+
+    if (Number(count) === 0) {
+      throw clientError('Route not found', 404);
+    }
+
+    await db('routes')
+      .where({ id: req.params.id })
+      .delete();
+
+    res.status(204).end();
   });
 }
 
@@ -79,6 +152,10 @@ export function routeCycleRoutes(config, logger, db) {
   router.get('/:id?', routeRead(config, logger, db));
 
   router.post('/', validate(add), routeCreate(config, logger, db));
+
+  router.patch('/:id', routeUpdate(config, logger, db));
+
+  router.delete('/:id', routeDelete(config, logger, db));
 
   return router;
 }
