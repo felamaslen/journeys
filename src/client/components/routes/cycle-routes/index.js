084a0a8d3ecc8fddd650c64360bf0ce8aa183446
Fela Maslen <felamaslen@gmail.com>
2019-04-12 21:46:55 +0100

    Basic list of cycle routes at /routes

--- src/client/components/routes/cycle-routes/index.js
+++ src/client/components/routes/cycle-routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import ListRoutes from '~/client/components/list-routes';
 import NewRoute from '~/client/components/route-editor/new-route';
 import ViewRoute from '~/client/components/route-editor/view-route';
 import EditRoute from '~/client/components/route-editor/edit-route';
@@ -12,6 +13,7 @@ export default function CycleRoutes() {
     <div className="cycle-routes">
       <h2 className="page-title">{'Cycle route editor'}</h2>
       <Switch>
+        <Route exact path="/routes" component={ListRoutes} />
         <Route path="/routes/new" component={NewRoute} />
         <Route path="/routes/:id/edit" component={EditRoute} />
         <Route path="/routes/:id" component={ViewRoute} />
