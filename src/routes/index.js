c5126de72a0a4177ccd200406ff9ad1034733ef2
Fela Maslen <felamaslen@gmail.com>
2019-04-13 11:16:56 +0100

    Added route to get origins and routes by origin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { routeCycleRoutes } from '~/routes/cycle-routes';
+import { routeSwitcher } from '~/routes/switcher';
 
 export function apiRoutes(config, logger, db) {
   const router = new Router();
 
   router.use('/routes', routeCycleRoutes(config, logger, db));
+
+  router.use('/origin', routeSwitcher(config, logger, db));
 
   return router;
 }
